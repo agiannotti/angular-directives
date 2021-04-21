f39e9c47b77f769cc0773672b6e1a01e900cee40
tonyg <aj.gianno@gmail.com>
2021-04-20 23:57:58 -0500

    organized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,6 +28,12 @@ export class AppComponent {
       { id: 1, name: 'course1' },
       { id: 2, name: 'course2' },
       { id: 3, name: 'course3' },
+      { id: 4, name: 'course4' },
+      { id: 5, name: 'course5' },
+      { id: 6, name: 'course6' },
+      { id: 7, name: 'course7' },
+      { id: 8, name: 'course8' },
+      { id: 9, name: 'course9' },
     ];
   }
 
